4f32ddf5f9466f7433f79b2c62383eae75038618
Marissa Schmidt <marissaschmidt@boisestate.edu>
2017-08-23 11:23:35 -0600

    jquery

--- site.js
+++ site.js
@@ -1,27 +1,69 @@
+
+// window.onload = function () {
+
+
+// 	document.getElementById("page-header").onclick = function() {
+// 		this.innerHTML = "Hello";
+// 	}
+
+// 	var paras = document.getElementsByTagName("p");
+
+// 	for(var i = 0; i < paras.length; i++) {
+// 		paras[i].onmouseover = function() {
+// 			this.style.color = "red";
+// 		}
+// 		paras[i].onmouseleave = function() {
+// 			this.style.color = "blue";
+// 		}
+
+// 	}
+	
+// }
+
+
+
 /* Plain Old JavaScript */
-window.onload = function() {
+// window.onload = function() {
 
-	var sections = document.getElementsByTagName('section');
+// 	// get all sections as array
+// 	var sections = document.getElementsByTagName('section');
 
-	for (var i = 0; i < sections.length; i++) {
-		sections[i].addEventListener("click", function() {
+// 	// go through all sections
+// 	for (var i = 0; i < sections.length; i++) {
+
+// 		// add click listener to each section
+// 		sections[i].addEventListener("click", function() {
+
+// 			console.log(this);
+// 			// find all but first child
+// 			var children = this.querySelectorAll(":not(:first-child)");
+// 			for(var j = 0; j < children.length; j++) {
+// 				if(children[j].style.display == 'none') {
+// 					children[j].style.display = 'block';
+// 				} else {
+// 					children[j].style.display = 'none';
+// 				}
+// 			}
+
+// 		});
+// 	}
+// };
+
 			//var content = this.getElementsByTagName('p')[0];
 			//var footer = this.querySelector('footer');
-			var children = this.querySelectorAll(":not(:first-child)");
-			for(var j = 0; j < children.length; j++) {
-				if(children[j].style.display == 'none') {
-					children[j].style.display = 'block';
-				} else {
-					children[j].style.display = 'none';
-				}
-			}
-		});
-	}
-};
 
 /* jQuery */
-// $(document).ready(function() {
-// 	$('section').click(function() {
-// 		$(this).children().not("h3").toggle();
-// 	});
-// });
+$(document).ready(function() {
+	$('section').click(function() {
+		$(this).children().not("h3").toggle();
+	});
+});
+
+
+
+
+
+
+
+
+
